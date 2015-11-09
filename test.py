23b5bc202c5b266500157b0bcf4be3bcdada8888
Jesse <1006412137@qq.com>
2015-11-10 02:28:35 +0800

    just for test

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+    
+'''main  function'''
+def  main():
+    print  '  "double quotation" in single quotation  .'  #  single  quotation  
+    print  "  'single quotation' in double quotation  ."  #  double  quotation 
+    
+if   __name__  ==  '__main__':
+    """   
+    call
+    main function
+    """
+    main()
